bdb9846cb796359efc1a4c41b2ba436fbbcdbf19
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-02-20 10:06:00 -0800

    delete functionality and fs.writeFile

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const uuid = require('uuid')
 const path = require('path')
 const notes = require('./db/db.json')
+const fs = require('fs')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -22,47 +23,42 @@ app.get('/notes', (req, res) => {
 
 app.get('/api/notes', (req, res) => res.json(notes))
 
-// app.get('/api/notes:id', (req, res) => {
-//     res.send(req.params.id)
-// })
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, res) => {
   const newNote = {
     id: uuid.v4(), //generate random id
     title: req.body.title,
     text: req.body.text,
   }
+  //require title/text input
   if (!newNote.title || !newNote.text) {
     return res.status(400).json({ msg: 'Please include title and text' })
   }
   notes.push(newNote)
+  fs.writeFile(
+    path.join(__dirname, 'db', 'db.json'),
+    JSON.stringify(notes),
+    (err) => {
+      if (err) throw err
+    }
+  )
   res.json(notes)
 })
-// //get single note
-// app.get('/api/notes/:id', (req, res) => {
-//     const found = notes.some((note) => note.id === req.params.id)
-//     if (found) {
-//       res.json({
-//         msg: 'Note deleted',
-//         notes: notes.filter((note) => note.id !== req.params.id),
-//       })
-//     } else {
-//       res.status(400).json({ msg: `No note with id ${req.params.id}` })
-//     }
-//   })
 
 //delete note
 app.delete('/api/notes/:id', (req, res) => {
   const found = notes.some((note) => note.id === req.params.id)
-  if (found) {
-    res.json({
-      msg: 'Note deleted',
-      notes: notes.filter((note) => note.id !== req.params.id),
-    })
-  } else {
-    res.status(400).json({ msg: `No note with id ${req.params.id}` })
-  }
+  if (!found) res.status(404).json({ msg: `No note with id ${req.params.id}` })
+
+  const index = notes.indexOf(found)
+  notes.splice(index, 1)
+  fs.writeFile(
+    path.join(__dirname, 'db', 'db.json'),
+    JSON.stringify(notes),
+    (err) => {
+      if (err) throw err
+    }
+  )
+  res.send(notes)
 })
 
 app.get('*', (req, res) => {
